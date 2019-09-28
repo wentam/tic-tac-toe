ebfe87f8176689dc5842fb7d40203ff5aee0cdeb
pegeler <paulegeler@gmail.com>
2019-09-28 03:26:01 -0400

    trying out pygame

---
+++ python/text.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+def draw(screen, pos, size, color, alignment, string):
+
+    font = pygame.font.Font(pygame.font.match_font('ubuntumono'), size)
+    font_dim = font.size(string)
+
+    if alignment[0] == "l":
+        x_offset = 0
+    elif alignment[0] == "c":
+        x_offset = font_dim[0] // 2
+    elif alignment[0] == "r":
+        x_offset = font_dim[0]
+    else:
+        raise Exception
+
+    y_offset = 0 if alignment[1] == "t" else font_dim[1]
+
+    screen.blit(font.render(string, True, color),
+                [pos[0] - x_offset, pos[1] - y_offset])
